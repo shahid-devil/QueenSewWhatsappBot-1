a7280ef54c3f77a3f96b5219626b495753c9c613
✬  DINUKA HIMSARA ✬ <93124334+dinukahimsara1@users.noreply.github.com>
2021-11-05 14:30:07 +0530

    Create SewAiMsg.js

---
+++ plugins/SewAiMsg.js
@@ -0,0 +1,246 @@
+/* Codded by @Ravindu Manoj
+Telegram: t.me/RavinduManoj
+Facebook: https://www.facebook.com/ravindu.manoj.79
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Whats bot - Ravindu Manoj
+*/
+
+const QueenSew = require('../events');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const https = require('https');
+const googleTTS = require('google-translate-tts');
+const { MessageType, Mimetype, MessageOptions } = require('@adiwajshing/baileys');
+const Language = require('../language');
+const Lang = Language.getString('voicy');
+const conf = require('../config');
+const axios = require('axios')
+const axiosdef = require("axios").default;
+const os = require('os')
+const translatte = require('translatte');
+const LanguageDetect = require('languagedetect');
+const lngDetector = new LanguageDetect();
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: conf.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + conf.HEROKU.APP_NAME;
+
+let wk = conf.WORKTYPE == 'public' ? false : true
+var vtalk_dsc = 'start to sew voice chat'
+var reply_sew = 'reply to any voice message'
+
+const recognizeAudio = () => {
+    const headers = new Headers({
+        'Content-Type': 'audio/wav',
+        "Authorization": `Bearer ${conf.WITAI_API}`,
+        'Cache-Control': 'no-cache',
+        'Transfer-Encoding': 'chunked'
+    })
+    const requestBody = {
+        method: "POST",
+        body: fs.readFileSync('output.wav'),
+        headers: headers
+    }
+    return fetch("https://api.wit.ai/speech?v=20200219", requestBody)
+        .then(response => response.json())
+        .then(json => json._text)
+}
+const convertToWav = file => {
+    return ffmpeg(file)
+        .audioCodec('pcm_s16le')
+        .format('wav')
+        .save('output.wav')
+}
+ if (conf.PSW !== 'kingraviya') {
+QueenSew.newcmdaddtosew({on: 'text', fromMe: wk, dontAdCommandList: true, deleteCommand: false}, (async (message, match) => {
+    if (message.message.startsWith('Sew') && conf.FULLSEW !== 'true') {        
+        var unique_ident = message.client.user.jid.split('@')[0]      
+      
+        var finm = message.message.replace('Sew', '').replace(' ', '')   
+      
+        var ldet = lngDetector.detect(finm)
+        var trmsg = ''
+        if (ldet[0][0] !== 'english') {
+            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+            if ('text' in ceviri) {
+                trmsg = ceviri.text
+            }
+        } else { trmsg = finm }
+        var uren = encodeURI(trmsg)
+        await axios.get('http://api.brainshop.ai/get?bid=158771&key=lde7pf4X5TyRxddQ&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+            var fins = ''                           
+            if (conf.LANG !== 'EN') {
+                ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                if ('text' in ceviri) {
+                    fins = ceviri.text
+                }
+            } else { fins = response.data.cnt }
+            await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+        })
+    }
+}));
+QueenSew.newcmdaddtosew({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+        if (conf.FULLSEW == 'true' && ((!message.jid.includes('-')) || (message.jid.includes('-') && 
+            (( message.mention !== false && message.mention.length !== 0 ) || message.reply_message !== false)))) {
+            if (message.jid.includes('-') && (message.mention !== false && message.mention.length !== 0)) {
+                message.mention.map(async (jid) => {
+                    if (message.client.user.jid.split('@')[0] === jid.split('@')[0]) {
+                        var unique_ident = message.client.user.jid.split('@')[0]      
+                        
+                        var finm = message.message
+                        var ldet = lngDetector.detect(finm)
+                        var trmsg = ''
+                        if (ldet[0][0] !== 'english') {
+                            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                            if ('text' in ceviri) {
+                                trmsg = ceviri.text
+                            }
+                        } else { trmsg = finm }
+                        var uren = encodeURI(trmsg)
+                        await axios.get('http://api.brainshop.ai/get?bid=158771&key=lde7pf4X5TyRxddQ&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+                            var fins = ''                           
+                            if (conf.LANG !== 'EN') {
+                                ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                                if ('text' in ceviri) {
+                                    fins = ceviri.text
+                                }
+                            } else { fins = response.data.cnt }
+                            await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+                        })
+                    }
+                })
+            } else if (message.jid.includes('-') && message.reply_message !== false) {
+                if (message.reply_message.jid.split('@')[0] === message.client.user.jid.split('@')[0]) {
+                    var unique_ident = message.client.user.jid.split('@')[0]      
+                    
+                    var finm = message.message
+                    var ldet = lngDetector.detect(finm)
+                    var trmsg = ''
+                    if (ldet[0][0] !== 'english') {
+                        ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                        if ('text' in ceviri) {
+                            trmsg = ceviri.text
+                        }
+                    } else { trmsg = finm }
+                    var uren = encodeURI(trmsg)
+                    await axios.get('http://api.brainshop.ai/get?bid=158771&key=lde7pf4X5TyRxddQ&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+                        var fins = ''                           
+                        if (conf.LANG !== 'EN') {
+                            ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                            if ('text' in ceviri) {
+                                fins = ceviri.text
+                            }
+                        } else { fins = response.data.cnt }
+                        await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+                    })
+                }
+            } else {
+                var unique_ident = message.client.user.jid.split('@')[0]      
+                
+                var finm = message.message
+                var ldet = lngDetector.detect(finm)
+                var trmsg = ''
+                if (ldet[0][0] !== 'english') {
+                    ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                    if ('text' in ceviri) {
+                        trmsg = ceviri.text
+                    }
+                } else { trmsg = finm }
+                var uren = encodeURI(trmsg)
+                await axios.get('http://api.brainshop.ai/get?bid=158771&key=lde7pf4X5TyRxddQ&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+                    var fins = ''                           
+                    if (conf.LANG !== 'EN') {
+                        ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                        if ('text' in ceviri) {
+                            fins = ceviri.text
+                        }
+                    } else { fins = response.data.cnt }
+                    await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+                })
+            }
+        }
+
+}));
+QueenSew.newcmdaddtosew({ pattern: 'vtalk$', desc: vtalk_dsc,dontAdCommandList: true, fromMe: wk }, (async (message, match) => {
+    if (!message.reply_message) return await message.client.sendMessage(message.jid,reply_sew, MessageType.text, { quoted: message.data }) 
+    try {
+        const file = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        })
+        
+        convertToWav(file)
+            .on('end', async () => {
+                const recognizedText = await recognizeAudio()
+                
+                var ssc = ''
+                ceviri = await translatte(recognizedText, {from: 'auto', to: 'EN' });
+                if ('text' in ceviri) {
+                    ssc = ceviri.text
+                }
+                var unique_ident = message.client.user.jid.split('@')[0]
+                
+        
+                var son = encodeURI(ssc)
+                await axios.get('http://api.brainshop.ai/get?bid=158771&key=lde7pf4X5TyRxddQ&uid=' + unique_ident + '&msg=' + son).then(async (response) => {
+                    var trmsg = ''
+                    cevir = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                    if ('text' in cevir) {
+                        trmsg = cevir.text
+                    }
+            
+                    let 
+                        LANG = conf.LANG.toLowerCase(),
+                        ttsMessage = trmsg,
+                        SPEED = 1.0
+                    var buffer = await googleTTS.synthesize({
+                        text: ttsMessage,
+                        voice: LANG
+                    });
+            
+                    await message.client.sendMessage(message.jid,buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data})
+                }).catch(async (error) => {
+	            console.log(error)
+                });
+        });
+    } catch (err) { console.log(err) }
+}));
+
+
+
+QueenSew.newcmdaddtosew({ pattern: 'sewai ?(.*)', desc: 'ai chat bot on off command' , fromMe: true, usage: '.fullsew on / off' }, (async (message, match) => {
+    var eva_status = `${Config.FULLSEW}`
+    if (match[1] == 'on') {
+        if (eva_status == 'true') {
+            return await message.client.sendMessage(message.jid, '*Already on 😒*', MessageType.text)
+        }
+        else {
+            await heroku.patch(baseURI + '/config-vars', { 
+                body: { 
+                    ['FULL_SEW']: 'true'
+                } 
+            });
+            await message.client.sendMessage(message.jid, '*Sew is now chat bot*', MessageType.text)
+        }
+    }
+    else if (match[1] == 'off') {
+        if (eva_status !== 'true') {
+            return await message.client.sendMessage(message.jid, '*chat bot already off*', MessageType.text)
+        }
+        else {
+            await heroku.patch(baseURI + '/config-vars', { 
+                body: { 
+                    ['FULL_SEW']: 'false'
+                } 
+            });
+            await message.client.sendMessage(message.jid, '*successful off ai chat bot*', MessageType.text)
+        }
+    }
+}));
+
+}
